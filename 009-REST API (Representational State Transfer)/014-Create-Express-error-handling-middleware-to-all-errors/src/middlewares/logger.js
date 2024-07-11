5b5b25d7d6b8253c81a3a34c9fc4bc2efbc16391
Jaydatt <124486498+jaydattpatel@users.noreply.github.com>
2024-07-11 13:23:50 +0530

    Add files via upload

---
+++ 009-REST API (Representational State Transfer)/014-Create-Express-error-handling-middleware-to-all-errors/src/middlewares/logger.js
@@ -0,0 +1,22 @@
+import winston from 'winston';
+
+const winLog = winston.createLogger({
+    level : "info",
+    format: winston.format.json(),
+    defaultMeta: { service: 'request-log' },
+    transports: [
+        new winston.transports.File({ filename: 'log.txt'}),
+    ],
+});
+
+const logger = async (req, res, next) => { 
+    //check if url not includes "users" text then log data. because we do not want to log email and password.
+    if(!req.url.includes("users")){
+        const logData = `Date: ${new Date().toString()}, URL: ${req.url}, Data: ${JSON.stringify(req.body)}`;
+        // log data in info level
+        winLog.info(logData);
+    }
+    next();
+};
+
+export default logger;
